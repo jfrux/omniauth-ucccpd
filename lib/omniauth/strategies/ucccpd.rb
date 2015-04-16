4942745342791bf1f38980e317433b99ab2c30b5
Joshua Rountree <joshua@swodev.com>
2015-04-16 16:19:16 -0400

    fixes

--- lib/omniauth/strategies/ucccpd.rb
+++ lib/omniauth/strategies/ucccpd.rb
@@ -7,22 +7,42 @@ module OmniAuth
       option :name, :ucccpd
 
       option :client_options, {
-        :site => "http://test.ccpd.uc.edu",
+        :site => "https://test.ccpd.uc.edu",
         :authorize_url => "/oauth/authorize"
       }
-      
-      uid { raw_info["id"] }
+
+      uid { raw_info['id'] }
 
       info do
         {
-          # :email => raw_info["email"]
-          # # and anything else you want to return to your API consumers
+          name: raw_info['certname'],
+          email: raw_info['default_email']['email_address'],
+          first_name: raw_info['firstname'],
+          last_name: raw_info['lastname'],
+          image: raw_info['profile_photo_url'],
+          display_name: raw_info['display_name']
         }
       end
 
-      def raw_info
-        @raw_info ||= access_token.get('/api/me').parsed
+      extra do
+        {'raw_info' => raw_info}
       end
+
+      # def token_params
+      #   options.token_params.merge(options_for('token'))
+      # end
+
+      def raw_info
+        @raw_info ||= access_token.get('/api/me', :headers => {'Accept' => "application/json; version=1" }).parsed['payload']['user'] || {}
+      end
+
+      # def build_access_token
+      #   super
+      # end
+
+      # def basic_auth_header
+      #   "Basic " + Base64.strict_encode64("#{options[:client_id]}:#{options[:client_secret]}")
+      # end
     end
   end
 end
